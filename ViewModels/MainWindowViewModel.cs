989968e1c4f636925d6b509f7897bdaf887f1c9e
alimratnurgajn@gmail.com <alimratnurgajn@gmail.com>
2025-08-25 15:23:08 +0500

    Revert "создана абстракный класс command, создан класс lambdaCommand и реализован сloseApplicationCommand который закрывает приложение"
    This reverts commit 3c578b919199c870c9aa74dd946947e5b669b2af.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,61 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
-using Al25.Infrastuctura.Commands;
-using Al25.ViewModels.Base;
 
 namespace Al25.ViewModels
 {
     internal class MainWindowViewModel
     {
-        #region Constructor 
-        public MainWindowViewModel()
-        {
-            // Инициализация команд
-            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
-        }
-        #endregion
-
-        #region Title string :
-        private string _title = "Тестовая программа Al25";
+        private string _title = "Тестовый проект Али";
         public string Title
         {
-            get => _title;
-            set { if (_title.Equals(value)) return; 
-                _title = value; }
-        }
-        #endregion
-        
-        #region Status string : 
-        /// <summary>
-        /// состояние программы
-        /// </summary>
-        private string _status = "Готов!";
-        /// <summary> состояние программы </summary>
-        public string Status
-        {
-            get => _status;
-            set
-            {
-                if (_status.Equals(value)) return;
-                _status = value;
+            get { return _title; }
+            set {
+                if (_title.Equals(value)) return;
+                _title = value; 
             }
         }
-        #endregion
-
-        #region Commands
-        public ICommand CloseApplicationCommand { get; }
-
-        private void OnCloseApplicationCommandExecuted(object p)
-        {
-            
-            System.Windows.Application.Current.Shutdown();
-        }
-
-        private bool CanCloseApplicationCommandExecute(object p) => true; // Здесь можно добавить логику проверки, если нужно
-
-        #endregion
-
     }
 }
