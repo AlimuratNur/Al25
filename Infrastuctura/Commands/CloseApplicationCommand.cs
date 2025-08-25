9eb541cd71572224ca5d5fc0dac29a617f1b9799
alimratnurgajn@gmail.com <alimratnurgajn@gmail.com>
2025-08-25 16:35:49 +0500

    определение класса отдельной команды

---
+++ Infrastuctura/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using Al25.Infrastuctura.Commands.Base;
+
+namespace Al25.Infrastuctura.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object paramenter) => Application.Current.Shutdown();
+    }
+}
