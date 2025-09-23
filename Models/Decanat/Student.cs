f276b71922d25aab507c9aed3a0c0bc7060ae95d
alimratnurgajn@gmail.com <alimratnurgajn@gmail.com>
2025-09-23 19:51:51 +0500

    Добавлен класс студенитов и их групп

---
+++ Models/Decanat/Student.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace Al25.Models.Decanat
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Patronymic { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public double Rating { get; set; }
+
+    }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+
+
+    }
+}
